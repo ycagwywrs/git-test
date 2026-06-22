520a7d08f597d39c6b30b6acd067e2bd24ba5503
admin <>
2026-06-22 11:41:19 +0900

    JavaScriptの追加と適用

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const heading = document.querySelector("h1");
+  heading.addEventListener("click", () => {
+    alert("JavaScriptの読み込みと動作確認完了");
+  });
+});
